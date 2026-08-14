14eafc67e6704a588b4d85d28bd24d51761fded2
snraju98 <nagarajusagiraju123@gmail.com>
2026-08-14 17:37:21 +0530

    jenkins

--- vars/testpipline.groovy
+++ vars/testpipline.groovy
@@ -1,6 +1,41 @@
 // This is function, by default if some one calls this pipeline like testPipeline call function will be executed
 def call (Map configMap){
     pipeline {
+        agent any
+        environment {
+            project = configMap.get("project")
+            component = configMap.get("component")
+        }
+        stages {
+            stage('Build') {
+                steps {
+                    script {
+                        sh """
+                            echo 'Building..'
+                            echo "Project: ${project}, component: ${component}"
+                            printenv | sort
+                        """
+                    }
+                    
+                }
+            }
+            stage('Test') {
+                steps {
+                    echo 'Testing..'
+                }
+            }
+            stage('Deploy') {
+                steps {
+                    echo 'Deploying....'
+                }
+            }
+        }
+    }
+}
+
+/* / This is function, by default if some one calls this pipeline like testPipeline call function will be executed
+def call (Map configMap){
+    pipeline {
        agent any
        environment {
            project = configMap.get("project")
@@ -32,4 +67,4 @@ def call (Map configMap){
         }
     } 
 
-}
+}/ */
